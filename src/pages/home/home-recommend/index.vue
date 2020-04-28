16e6994dc97ac96295a943c21401d9f4322335bd
“Vimalate <1902644369@qq.com>
2020-04-28 17:20:45 +0800

    recommend

--- src/pages/home/home-recommend/index.vue
+++ src/pages/home/home-recommend/index.vue
@@ -1,9 +1,115 @@
 <template>
-  <view class="content">推荐</view>
+  <view class="content" v-if="recommends.length>0">
+    <!-- 推荐 -->
+    <view class="recommend_wrap">
+      <view class="recommend_item" v-for="(item) in recommends" :key="item.id">
+        <img :src="item.thumb" mode="widthFix" alt />
+      </view>
+    </view>
+    <!-- 日期 -->
+    <view class="month_wrap">
+      <view class="month_title">
+        <view class="month_title_info">
+          <view class="month_title">
+            <text>{{monthes.DD}} /</text>
+            {{monthes.MM}}月
+          </view>
+          <view class="month_text">清风拂晓，是夏</view>
+        </view>
+        <view class="month_title_more">更多 ></view>
+      </view>
+      <view class="month_content">
+        <view class="month_item" v-for="(item) in monthes.items" :key="item.id">
+          <img mode="aspectFill" :src="item.thumb+item.rule.replace('$<Height>',360)" alt />
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      // 推荐
+      recommends: [],
+      monthes: []
+    };
+  },
+  mounted() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      this.request({
+        url: "http://service.picasso.adesk.com/v3/homepage/vertical",
+        data: {
+          limit: 30,
+          order: "hot",
+          skip: 0
+        }
+      }).then(res => {
+        console.log(res);
+        // 推荐
+        this.recommends = res.res.homepage[1].items;
+        // 月份
+        this.monthes = res.res.homepage[2];
+        this.monthes.MM = moment(this.monthes.stime).format("MM");
+        this.monthes.DD = moment(this.monthes.stime).format("DD");
+      });
+    }
+  }
+};
 </script>
 
 <style lang='scss'>
+.recommend_wrap {
+  display: flex;
+  flex-wrap: wrap;
+  .recommend_item {
+    width: 50vw;
+    border: 5rpx solid #fff;
+  }
+}
+.month_wrap {
+  .month_title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10rpx;
+    .month_title_info {
+      display: flex;
+      align-items: center;
+      color: $color;
+      font-size: 30rpx;
+      font-weight: 600;
+      .month_title {
+        text {
+          font-size: 36rpx;
+        }
+      }
+
+      .month_text {
+        font-size: 34rpx;
+        color: #666;
+        margin-left: 30rpx;
+      }
+    }
+
+    .month_title_more {
+      font-size: 24rpx;
+      color: $color;
+    }
+  }
+
+  .month_content {
+    display: flex;
+    flex-wrap: wrap;
+    .month_item {
+      width: 33.333%;
+      border: 3rpx solid #fff;
+    }
+  }
+}
 </style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,27 +1,83 @@
 <template>
   <view class="home">
-    <home-album></home-album>
-    <home-recommend></home-recommend>
-    <home-new></home-new>
-    <home-category></home-category>
+    <view class="home_tab">
+      <view class="home_tab_title">
+        <view class="home_tab_inner">
+          <uni-segmented-control
+            :current="current"
+            :values="items.map(v=>v.title)"
+            @clickItem="onClickItem"
+            style-type="text"
+            active-color="#d4237a"
+          ></uni-segmented-control>
+        </view>
+        <view class="iconfont iconsearch"></view>
+      </view>
+      <view class="home_tab_content">
+        <view v-if="current === 0">
+          <home-recommend></home-recommend>
+        </view>
+        <view v-if="current === 1">
+          <home-category></home-category>
+        </view>
+        <view v-if="current === 2">
+          <home-new></home-new>
+        </view>
+        <view v-if="current === 3">
+          <home-album></home-album>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
-import homeAlbum from './home-album'
-import homeRecommend from './home-recommend'
-import homeNew from './home-new'
-import homeCategory from './home-category'
-import {uniSegmentedControl} from '@dcloudio/uni-ui'
-export default{
-  components:{
+import homeAlbum from "./home-album";
+import homeRecommend from "./home-recommend";
+import homeNew from "./home-new";
+import homeCategory from "./home-category";
+import { uniSegmentedControl } from "@dcloudio/uni-ui";
+export default {
+  data() {
+    return {
+      items: [
+        { title: "推荐" },
+        { title: "分类" },
+        { title: "最新" },
+        { title: "专辑" }
+      ],
+      current: 0
+    };
+  },
+  methods: {
+    onClickItem(e) {
+      if (this.current !== e.currentIndex) {
+        this.current = e.currentIndex;
+      }
+    }
+  },
+  components: {
     homeAlbum,
     homeRecommend,
     homeNew,
-    homeCategory
-  }
-}
+    homeCategory,
+    uniSegmentedControl
+  },
+  
+};
 </script>
 
 <style lang='scss'>
+.home_tab {
+  .home_tab_title {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    .home_tab_inner {
+      width: 70vw;
+    }
+    .iconsearch {
+    }
+  }
+}
 </style>
