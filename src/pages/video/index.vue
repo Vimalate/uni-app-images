66e8590f70372a0ed75dfa7625d62417b39d91b9
“Vimalate <1902644369@qq.com>
2020-04-30 18:47:40 +0800

    video

--- src/pages/video/index.vue
+++ src/pages/video/index.vue
@@ -14,9 +14,13 @@
         <view class="iconfont iconsearch"></view>
       </view>
       <view class="video_tab_content">
-		  <view v-if="current<4"><video-main></video-main></view>
-		  <view v-if="current===4"><video-category></video-category></view>
-	  </view>
+        <view v-if="current<4">
+          <video-main></video-main>
+        </view>
+        <view v-if="current===4">
+          <video-category></video-category>
+        </view>
+      </view>
     </view>
   </view>
 </template>
@@ -27,17 +31,38 @@ import videoMain from "./videoMain";
 import videoCategory from "./videoCategory";
 export default {
   data() {
-	//   https://service.picasso.adesk.com/v2/wallpaper/wallpaper/${id}/comment
-	// http://157.122.54.189:9088/videoimg/v1/videowp/category
-	//  https://service.picasso.adesk.com/videoimg/v1/videowp/category
-	//  https://service.picasso.adesk.com/v1/videowp/category
+    //   https://service.picasso.adesk.com/v2/wallpaper/wallpaper/${id}/comment
+    // http://157.122.54.189:9088/videoimg/v1/videowp/category
+    //  https://service.picasso.adesk.com/videoimg/v1/videowp/category
+    //  https://service.picasso.adesk.com/v1/videowp/category
     return {
       items: [
-        { title: "推荐" ,url:'https://service.picasso.adesk.com/videoimg/v1/videowp/featured',params: { limit: 30, skip: 0, order: 'hot' }},
-        { title: "娱乐" },
-        { title: "最新" },
-        { title: "热门" },
-        { title: "分类" }
+        {
+          title: "推荐",
+          url: "http://157.122.54.189:9088/videoimg/v1/videowp/featured",
+          params: { limit: 30, skip: 0, order: "hot" }
+        },
+        {
+          title: "娱乐",
+          url:
+            "http://157.122.54.189:9088/videoimg/v1/videowp/category/59b25abbe7bce76bc834198a",
+          params: { limit: 30, skip: 0, order: "new" }
+        },
+        {
+          title: "最新",
+          url: "http://157.122.54.189:9088/videoimg/v1/videowp/videowp",
+          params: { limit: 30, skip: 0, order: "new" }
+        },
+        {
+          title: "热门",
+          url: "http://157.122.54.189:9088/videoimg/v1/videowp/videowp",
+          params: { limit: 30, skip: 0, order: "hot" }
+        },
+        {
+          title: "分类",
+          url: "http://157.122.54.189:9088/videoimg/v1/videowp/category",
+          params: {}
+        }
       ],
       current: 0
     };
